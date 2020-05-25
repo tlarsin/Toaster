ac391ecfdb73612d9b76b0af09c5b6149fa58f99
Tristan Larsin <tlarsin@gmail.com>
2020-05-25 16:04:52 -0500

    Initial commit.

---
+++ Toaster/app/src/main/java/io/tristanlarsin/toaster/ToasterExample.kt
@@ -0,0 +1,31 @@
+package io.tristanlarsin.toaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ToasterExample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_bottom.setOnClickListener {
+            Toaster(this)
+                .setTextSize(12f)
+                .setBackgroundColor(R.color.colorAccent)
+                .setTextPadding(40)
+                .setRadius(50)
+                .setLength(Toaster.LENGTH_SHORT)
+                .show("Bottom Toast")
+        }
+
+        btn_top.setOnClickListener {
+            Toaster(this)
+                .setTextSize(20f)
+                .align(Toaster.ALIGN_TOP, 0, 200)
+                .setTextColor(R.color.white)
+                .setLength(Toaster.LENGTH_LONG)
+                .show("Top Toast")
+        }
+    }
+}
---
+++ Toaster/toaster/src/main/java/io/tristanlarsin/toaster/Toaster.kt
@@ -0,0 +1,127 @@
+package io.tristanlarsin.toaster
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.setPadding
+
+@SuppressLint("InflateParams")
+class Toaster(private val context: Context) {
+
+    private val toast: Toast
+    private val view: View
+
+    private var length = LENGTH_SHORT
+    private var typeFace = Typeface.DEFAULT
+    private var textColor = Color.WHITE
+    private var textSize = 16f
+    private var textPadding = 10
+
+    private var cardElevation = 8
+    private var cardBackgroundColor = Color.BLACK
+    private var cardRadius = 8
+
+    private val inflater by lazy {
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    }
+
+    init {
+        view = inflater.inflate(R.layout.toast_layout, null)
+        val toast = Toast(context)
+        toast.duration = LENGTH_SHORT
+        toast.view = view
+        this.toast = toast
+    }
+
+    fun show(message: String) {
+        toastView.findViewById<TextView>(R.id.tv_message).text = message
+        setParams()
+        toast.show()
+    }
+
+    // Set text size
+    fun setTextSize(size: Float) : Toaster {
+        textSize = size
+        return this
+    }
+
+    // Set Text Color
+    fun setTextColor(color: Int) : Toaster {
+        textColor = ContextCompat.getColor(context, color)
+        return this
+    }
+
+    // Set Text Padding
+    fun setTextPadding(padding: Int) : Toaster {
+        textPadding = padding
+        return this
+    }
+
+    // Set background color
+    fun setBackgroundColor(color: Int) : Toaster {
+        cardBackgroundColor = color
+        return this
+    }
+
+    // Set radius
+    fun setRadius(radius: Int) : Toaster {
+        cardRadius = radius
+        return this
+    }
+
+    fun setElevation(elevation: Int) : Toaster {
+        cardElevation = elevation
+        return this
+    }
+
+    // Align Toast
+    fun align(value: Int, horizontalOffset: Int = 0, verticalOffset: Int = 0) : Toaster {
+        toast.setGravity(value, horizontalOffset, verticalOffset)
+        return this
+    }
+
+    // Set Toast Duration
+    fun setLength(length: Int) : Toaster {
+        this.length = length
+        return this
+    }
+
+    // Call before showing
+    private fun setParams() {
+        textView.setPadding(textPadding)
+        textView.textSize = textSize
+        textView.typeface = typeFace
+        textView.setTextColor(textColor)
+
+        cardView.setCardBackgroundColor(cardBackgroundColor)
+        cardView.radius = cardRadius.toFloat()
+        cardView.cardElevation = cardElevation.toFloat()
+
+        toast.duration = length
+    }
+
+    private val toastView = toast.view
+    private val textView = toastView.findViewById<TextView>(R.id.tv_message)
+    private val cardView = toastView.findViewById<CardView>(R.id.card_view)
+
+    companion object {
+        // Toast duration
+        const val LENGTH_SHORT = Toast.LENGTH_SHORT
+        const val LENGTH_LONG = Toast.LENGTH_LONG
+
+        // Toast position
+        const val ALIGN_TOP = Gravity.TOP
+        const val ALIGN_BOTTOM = Gravity.BOTTOM
+        const val ALIGN_START = Gravity.START
+        const val ALIGN_END = Gravity.END
+        const val CENTER = Gravity.CENTER
+    }
+}
